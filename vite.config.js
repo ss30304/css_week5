644bc8a1bad2f162787542b42282f66e59c5b341
ss30304 <ss3030421@gmail.com>
2023-09-10 21:41:08 +0800

    update commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "https://github.com/ss30304/css_week5.git",
+  base: "/css_week5/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
